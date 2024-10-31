5287d8fb7300d211633a741aabed3427abfead0a
Daniela Londono <daniela0393@gmail.com>
2024-10-31 08:44:49 -0500

    feat: login design

---
+++ src/app/core/application/ports/auth.port.ts
@@ -0,0 +1,10 @@
+import { ILoginRequest, ILoginResponse } from "../dto";
+
+export interface PAuth {
+  /**
+   * 
+   * @param req 
+   */
+  login(req: ILoginRequest): Promise<ILoginResponse>
+  
+}
